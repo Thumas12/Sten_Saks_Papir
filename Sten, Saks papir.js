52a26a2003d3e6d7d1d63a980c3a434b79b0f2af
Thumas12 <thch19ab@student.cbs.dk>
2019-09-29 17:26:05 +0200

    sten

---
+++ Sten, Saks papir.js
@@ -0,0 +1,48 @@
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'sten'||userInput =='papir'|| userInput == 'saks'){
+        return userInput;
+    } else {
+        console.log("Fejl");
+    }
+}
+const getComputerChoice = () =>{
+    const randomNumber =          Math.floor(Math.random() * 3);
+    switch (randomNumber){
+        case 0:
+            return 'sten';
+        case 1:
+            return 'papir';
+        case 2:
+            return 'saks';
+    }
+}
+const determineWinner = (userChoice, computerChoice) =>{
+    if (userChoice === computerChoice){
+        return "Uafgjort"
+    } if (userChoice === 'sten'){
+        if (computerChoice === 'papir'){
+            return "Computeren vandt"
+        }
+        else {
+            return "du vandt"}
+    } if (userChoice === 'saks'){
+        if(computerChoice === 'sten'){
+            return "Computeren vinder"
+        } else{
+            return "Du vandt"}
+    } if (userChoice === 'papir'){
+        if (computerChoice === 'saks'){
+            return 'computeren vandt'
+        } else {
+            return 'Du vandt'}
+    }
+}
+const playGame = () => {
+    const userChoice = getUserChoice('sten');
+    const computerChoice = getComputerChoice();
+    console.log(`Du valgte: ${userChoice}`)
+    console.log(`Computeren valgte : ${computerChoice}`)
+    console.log(determineWinner(userChoice, computerChoice))
+}
+playGame()
